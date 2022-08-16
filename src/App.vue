db5fefbf8b369320f983f293e9001d17bbea7689
Your Name <you@example.com>
2022-08-16 14:33:50 +0300

    optimisation

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,92 @@ export default {
   name: "App",
   data() {
     return {
-      postResult: null,
+      player: "X",
+      table: false,
+      gameId: 0,
+      winner: false,
+      gameFinished: false,
+      moves: [],
     };
   },
+  mounted() {
+    this.resetTable();
+    this.getData();
+  },
   methods: {
-    fortmatResponse(res) {
-      return JSON.stringify(res, null, 2);
+    calculateWinner(squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+          squares[a] &&
+          squares[a] === squares[b] &&
+          squares[a] === squares[c]
+        ) {
+          return squares[a];
+        }
+      }
+      return null;
     },
+    resetTable() {
+      this.table = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+      ];
+    },
+    reset() {
+      this.winner = false;
+      this.gameId = 0;
+      this.resetTable();
+      this.gameFinished = false;
+      this.moves = [];
+    },
+    move(x, y) {
+      if (this.gameFinished) {
+        return;
+      }
+      if (this.table[x][y] !== "") return;
+      this.table[x][y] = this.player;
+      this.player = this.player === "X" ? "O" : "X";
+      this.postData();
+    },
+    async getData() {
+      let lastGameId = sessionStorage.getItem("lastGameId");
+      if (!lastGameId) {
+        return;
+      }
+
+      try {
+        let response = await fetch(
+          `http://localhost:5000/moves?gameId=${lastGameId}`
+        );
+        let moves = await response.json();
+        let lastMoves = moves[moves.length - 1];
+        this.gameId = lastMoves.gameId;
+        this.table = JSON.parse(lastMoves.move);
+        this.moves = moves;
+        console.log(this.moves);
+      } catch (error) {}
+    },
+
     async postData() {
+      if (this.gameId === 0) {
+        this.gameId = Date.now();
+        sessionStorage.setItem("lastGameId", this.gameId);
+      }
       const postData = {
-        move: document.getElementById("trackMovesID").innerHTML,
+        move: JSON.stringify(this.table),
+        gameId: this.gameId,
       };
       try {
         const res = await fetch(`${baseURL}`, {
@@ -31,83 +107,19 @@ export default {
         const result = {
           data: data,
         };
-        this.postResult = this.fortmatResponse(result);
-      } catch (err) {
-        this.postResult = err.message;
-      }
+        this.moves.push(data);
+        console.log(data);
+
+        let winner = this.calculateWinner(this.table.flat());
+        if (winner !== null) {
+          this.winner = winner;
+          this.gameFinished = true;
+        }
+      } catch (err) {}
     },
     clearPostOutput() {},
   },
 };
-</script>
-<!--  -->
-<script setup>
-import { ref, computed, watch, onMounted } from "vue";
-import TrackMove from "./components/TrackMove.vue";
-
-const player = ref("X");
-const table = ref([
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-]);
-
-const CalculateWinner = (squares) => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-};
-
-const winner = computed(() => CalculateWinner(table.value.flat()));
-
-const Move = (x, y) => {
-  if (winner.value) return;
-  if (table.value[x][y] !== "") return;
-  table.value[x][y] = player.value;
-  player.value = player.value === "X" ? "O" : "X";
-
-  // to consoleLog
-  const trackMoves = table.value.flat().toString();
-  document.getElementById("trackMovesID").innerHTML = trackMoves;
-  console.log(trackMoves);
-};
-
-const Reset = () => {
-  table.value = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
-  player.value = "X";
-};
-
-// sessionStorage wins
-
-const history = ref([]);
-watch(winner, (current, previous) => {
-  if (current && !previous) {
-    history.value.push(current);
-    sessionStorage.setItem("history", JSON.stringify(history.value));
-  }
-});
-
-onMounted(() => {
-  history.value = JSON.parse(sessionStorage.getItem("history")) ?? [];
-});
 </script>
 
 <template>
@@ -119,7 +131,7 @@ onMounted(() => {
         <div
           v-for="(cell, y) in row"
           :key="y"
-          @click="Move(x, y)"
+          @click="move(x, y)"
           :class="`border border-black w-24 h-24 hover:bg-gray-200 duration-300 flex items-center justify-center material-icons-outlined text-4xl cursor-pointer ${
             cell === 'X' ? 'text-lime-400' : 'text-cyan-400'
           }`"
@@ -129,45 +141,18 @@ onMounted(() => {
       </div>
     </div>
     <h2 v-if="winner" class="text-6xl font-bold mb-8">
-      Player '<span class="text-yellow-500">{{ winner }}</span
-      >' wins
+      Player <span class="text-yellow-500">{{ winner }}</span> wins
     </h2>
     <button
-      @click="Reset"
+      @click="reset()"
       class="px-4 py-2 bg-cyan-300 rounded uppercase font-bold hover:bg-cyan-500 duration-300"
     >
       Reset
     </button>
-
-    <!-- sesions -->
-
-    <h2 class="mt-5 text-4xl">History</h2>
-    <div v-for="(game, idx) in history" :key="idx">
-      Game{{ idx + 1 }}: {{ game }} won
-    </div>
-
-    <!-- moves  -->
-
-    <div>
-      <p class="mt-5 text-4xl">Moves made</p>
-      <p class="mt-5 text-1xl" id="trackMovesID"></p>
-      <!-- Post data to BE -->
-      <div id="app" class="content-start">
-        <div>
-          <div class="mt-5 text-4xl">Vue Fetch POST moves</div>
-          <div class="rounded-sm">
-            <button
-              class="mt-5 px-4 py-2 bg-cyan-300 rounded uppercase font-bold hover:bg-cyan-500 duration-300"
-              @click="postData"
-            >
-              Post Data
-            </button>
-            <TrackMove />
-          </div>
-        </div>
-      </div>
+    <div v-for="move in moves" :key="move" class="move">
+      <p>
+        {{ move.move }}
+      </p>
     </div>
   </main>
 </template>
-
-<style></style>
